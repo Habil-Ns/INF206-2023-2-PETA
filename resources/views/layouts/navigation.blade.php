1abc4a579d18832e0786428e062ed3f9bc8919ca
Habil-Ns <habil_ns@mhs.unsyiah.ac.id>
2023-04-02 21:28:59 +0700

    feat: menambahkan beranda

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,36 @@
+<header class="head" id="header">
+        <nav class="container rows navigation">
+            <div class="nav_brand">
+                    <span>PETA</span>-Pemandu Wisata
+            </div>
+            <div class="nav_menu" id="nav-menu">
+                <ul class="nav_list">
+                    <li class="nav_item">
+                        <a href="#" class="nav_link active-link">Beranda</a>
+                    </li>
+                    <li class="nav_item">
+                        <a href="#" class="nav_link">Sejarah</a>
+                    </li>
+                    <li class="nav_item">
+                        <a href="#" class="nav_link">Galeri</a>
+                    </li>
+                    <li class="nav_item">
+                        <a href="#" class="nav_link">Wisata Banda Aceh</a>
+                    </li>
+                    <li class="nav_item">
+                        <a href="#" class="nav_link">Pemandu Wisata</a>
+                    </li>
+                    <li class="nav_item">
+                        <a href="#" class="nav_link">Hubungi Kami</a>
+                    </li>
+                    <li class="nav_item">
+                        <a href="#" class="nav_link">
+                            <img src="image/user.png" alt="profile" width=56px height=56px>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        @yield('content')
+    </header>
