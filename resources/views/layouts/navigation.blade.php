ca012b803cc855b5089dd7e52e2740d1c3348532
Fanul1 <fanul@mhs.unsyiah.ac.id>
2023-04-07 15:05:03 +0700

    feat: tambah notif di user pemandu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -7,6 +7,14 @@
     <title>@yield('title')</title>
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+    <!-- BootStrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <!-- JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- Popper --> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <!-- BootStrap JS -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> 
 <body>
 <header class="head" id="header">
         <nav class="container rows navigation">
@@ -34,9 +42,18 @@
                         <a href="#" class="nav_link">Hubungi Kami</a>
                     </li>
                     <li class="nav_item">
-                        <a href="#" class="nav_link">
-                            <img src="{{ asset('images/user.png') }}" alt="User" width=56px height=56px>
+                        <a href="#" class="nav_link" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img src="{{ asset('images/profile_pemandu.jpg') }}" alt="User" width=56px height=56px>
                         </a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                            <button class="dropdown-item" href="#">Profil</button>
+                            <button class="dropdown-item" href="#">
+                                Notifikasi <span class="badge badge-light">1</span>
+                                <span class="sr-only">unread messages</span>
+                            </button>
+                            <div class="dropdown-divider"></div>
+                                <button class="dropdown-item" href="#">Keluar</button>
+                          </div>
                     </li>
                 </ul>
             </div>
