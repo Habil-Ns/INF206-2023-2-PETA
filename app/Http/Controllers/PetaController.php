8f5d938cf9997c74961e288349459aeb8cfdc533
Fanul1 <102352999+Fanul1@users.noreply.github.com>
2023-04-17 11:34:27 +0700

    Update PetaController.php

--- app/Http/Controllers/PetaController.php
+++ app/Http/Controllers/PetaController.php
@@ -30,7 +30,7 @@ class PetaController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store()
     {
         return view('peta.hubungikami');
     }
