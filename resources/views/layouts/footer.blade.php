90d6c65be162c327af0d9e10d4234063d5a31092
Askar Aziz <askaraziz251120@gmail.com>
2023-05-02 22:59:19 +0700

    feat:membuat footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        <link href='https://fonts.googleapis.com/css?family=Patua One' rel='stylesheet'>
+        <link href='https://fonts.googleapis.com/css?family=Source Serif Pro' rel='stylesheet'>
+    <title>Document</title>
+</head>
+<style>
+    #footer {
+        background-color: #1BB580;
+    }
+
+    #f1 {
+        display: flex;
+        justify-content: end;
+        margin-top: 2px;
+    }
+</style>
+
+<body>
+
+    <!-- Footer -->
+    <footer class="">
+        <section class="" id="footer">
+            <div class="container text-center text-md-start mt-5 ">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <h6 class="text-uppercase fw-bold mb-4" style="font-family: 'Patua One'; margin-top: 2px;">
+                            <i class=""></i>PETA-Pemandu Wisata
+                        </h6>
+                        <p style="font-family: 'Source Serif Pro';">
+                            Tujuan kami adalah memfasilitasi para wisatawan dalam mencari pemandu wisata di Banda Aceh
+                            dan membuka kesempatan kerja bagi mereka yang ingin menjadi pemandu wisata
+                        </p>
+                    </div>
+                    <div class="col-sm-9" id="f1">
+                        <div class="row">
+                            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                                <h6 class="text-uppercase fw-bold mb-4">
+                                    <a href="" class="text-decoration-none text-black" style="font-family: 'Patua One';">pemandu wisata </a>
+                                </h6>
+                            </div>
+                            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                                <h6 class="text-uppercase fw-bold mb-4">
+                                    <a href="" class="text-decoration-none text-black" style="font-family: 'Patua One';">Pendaftaran pemandu </a>
+                                </h6>
+                            </div>
+                            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                                <h6 class="text-uppercase fw-bold mb-4">
+                                    <a href="" class="text-decoration-none text-black" style="font-family: 'Patua One';">Hubungi </a>
+                                </h6>
+                            </div>
+                        </div>
+
+
+                        
+                    </div>
+                    <div class="text-center p-3" style="font-family: 'Source Serif Pro';">
+                        Copyright © 2023 PETA-Pemandu Wisata
+                    </div>
+                </div>
+        </section>
+
+
+    </footer>
+    <!-- Footer -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
